2d52365120ccebda5b7608b3023b6683539116c3
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-28 14:58:22 +0530

    [vidyadhar hudge]Add:Terissa is a part of sales and marketing department

---
+++ TerissaPartOfSalesAndMarketingDept.sql
@@ -0,0 +1,3 @@
+insert into employee_payroll 
+values(104,'Terissa',400000.00,'2020-06-1','F',8156782910,'Sales','mumbai',2000,1000,200,18000),
+(105,'Terissa',400000.00,'2020-06-1','F',8156782910,'Marketing','mumbai',2000,1000,200,18000);
