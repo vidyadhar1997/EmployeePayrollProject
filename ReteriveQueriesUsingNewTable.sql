9b9074f70abc4fa02c29864483667eebcc4a4214
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-29 08:45:59 +0530

    [vidyadhar hudge]Refactor:Retrieve data using new table

--- ReteriveQueriesUsingNewTable.sql
+++ ReteriveQueriesUsingNewTable.sql
@@ -1,9 +1,15 @@
-/*UC-4*/
+/*UC--4*/
 select * from employee e inner join company c on 
 e.company_id=c.company_id inner join payroll p on
 p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
-e.employee_id=ed.employee_Department_id inner join Department d on
-ed.employee_Department_id=d.department_id where e.employee_id=1;
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=1;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=2;
 
 /*UC-5*/
 select basic_pay from employee where start_date between CAST('2020-10-2' as date) and GETDATE();
